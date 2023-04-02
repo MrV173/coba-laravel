c82e94fdb37bad028f8173ec4f8ffb9950fac5d6
MrV173 <ibnuhakim81@gmail.com>
2023-04-02 11:28:06 +0700

    laravel8

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.main')
+@section('container')
+<style>
+    body{
+        background-image: url("https://thumbs.dreamstime.com/b/wood-background-wallpaper-hd-123273980.jpg");
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
+</style>
+    <center>
+        <h1> VZONE WEBSITE </h1>
+    </center>
+    </body>
+@endsection
+    
